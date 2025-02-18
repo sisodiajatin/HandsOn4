393c596e83002d3a9bf1631b3b1d83713c37b9ab
Jatin Sisodia <sisodia1999@gmail.com>
2025-02-17 21:00:41 -0500

    Changed Hello World to Hi

--- obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed921ce7a4ce60cd4e5b3767fd1d99c97ab5ba32")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
